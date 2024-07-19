b8b701411a7b3de4d2d969e78d2d48bc8e774e7d
Quillzyy <shaquirearija1@gmail.com>
2024-07-19 14:50:56 +0700

    Add userlist

--- web/tst.py
+++ web/tst.py
@@ -1,39 +1,22 @@
-from flask import Flask, render_template, request, jsonify
+import subprocess
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
-
-# In-memory user list for demonstration purposes
-users = [{'id': 1, 'name': 'User1'}, {'id': 2, 'name': 'User2'}, {'id': 3, 'name': 'User3'}]
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/add_user', methods=['POST'])
-def add_user():
-    new_user = request.json
-    users.append(new_user)
-    return jsonify({'status': 'success', 'user': new_user})
-
 @app.route('/get_users')
 def get_users():
-    return jsonify(users)
+    try:
+        group_id = subprocess.check_output("getent group safetp | cut -d: -f3", shell=True).decode('utf-8').strip()
+        users = subprocess.check_output(f"cat /etc/passwd | grep {group_id} | cut -d: -f1", shell=True).decode('utf-8').split()
+        user_list = [{'id': i + 1, 'name': user} for i, user in enumerate(users)]
+    except subprocess.CalledProcessError:
+        user_list = []
 
-@app.route('/edit_user', methods=['POST'])
-def edit_user():
-    updated_user = request.json
-    for user in users:
-        if user['id'] == int(updated_user['id']):
-            user['name'] = updated_user['name']
-            break
-    return jsonify({'status': 'success', 'user': updated_user})
-
-@app.route('/delete_user', methods=['POST'])
-def delete_user():
-    user_id = int(request.json['id'])
-    global users
-    users = [user for user in users if user['id'] != user_id]
-    return jsonify({'status': 'success'})
+    return jsonify(user_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
